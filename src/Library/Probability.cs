5dae45917b77e609adcdde279a637cff443a0c9f
[Franco Barlocco] <[titobarlocco52@gmail.com]>
2022-06-16 14:34:28 -0300

    cambios

--- src/Library/Probability.cs
+++ src/Library/Probability.cs
@@ -4,8 +4,8 @@ namespace Library
     {
         public static Dictionary<int, string> selectedCards = new Dictionary<int, string>();
         public static List<string> posiciones = new List<string>();
-
-        public static int Factorial(int n)
+        /*
+        public static float Factorial(int n)
         {
             {
                 int fact = 1;
@@ -13,15 +13,32 @@ namespace Library
                 {
                     fact *= i;
                 }
+                Console.WriteLine("El Factorial de " + n + " es: " + fact);
                 return fact;
-                //Console.WriteLine("\nEl Factorial de " + n + " es: " + fact);
+                
             }
+            
+        }
+        */
+
+        public static double Factorial(int n)
+        {
+            
+            if((n== 0) || (n==1))
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Factorial(n-1);
+            }
+
         }
 
         //Combinaciones donde numA es el numero del total y numB en cuanto quiero tomar las variaciones.
-        public static int Combinacion(int numA, int numB)
+        public static double Combinacion(int numA, int numB)
         {
-            int result = Factorial(numA) / Factorial(numA - numB) * Factorial(numB);
+            double result = (Factorial(numA) / (Factorial(numA - numB) * Factorial(numB)));
 
             return result;
         }
@@ -30,8 +47,13 @@ namespace Library
 
             //int C12In4 = Combinacion(12,5);
             // int C52In5 = Combinacion(52,5);
+            //float primeraSuma = 2*(((Combinacion(12,4)*38)/(Combinacion(52,5))) + (Combinacion(12,5)/Combinacion(52,5)));
+            //float segundaSuma = 2*((Combinacion(26,5)/Combinacion(52,5)));
 
-            float colorProbability = 2 * ((Combinacion(12, 4) * 38 + Combinacion(12, 5)) + 2 * Combinacion(26, 5));
+            double colorProbability = 2 * (((Combinacion(12, 4) * 38) + Combinacion(12, 5)) + Combinacion(26, 5));
+            colorProbability = colorProbability*(Combinacion(52,5));
+
+            //float colorProbability = primeraSuma + segundaSuma;
 
             Console.WriteLine($"La probabilidad de obtener una jugada color es {colorProbability}");
         }
